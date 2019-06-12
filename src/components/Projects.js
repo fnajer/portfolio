a1d9a4448babbe2c9a14c45c37f0c0bcdc93f3eb
fnajer <lexactr@gmail.com>
2019-06-12 15:18:58 +0300

    simplify components without state to functional syntax

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,35 +1,27 @@
-import React, { Component } from 'react';
-import PROJECTS from '../data/projects';
+import React from "react";
+import PROJECTS from "../data/projects";
 
-class Project extends Component {
-  render() {
-    const { title, image, description, link } = this.props.project;
-    return (
-      <div style={{ display: 'inline-block', width: 300, margin: 10 }}>
-        <h2>{title}</h2>
-        <img src={image} alt="Example app" style={{ width: 200, height: 120 }}/>
-        <p>{description}</p>
-        <a href={link}>{link}</a>
-      </div>
-    )
-  }
-}
+const Project = props => {
+  const { title, image, description, link } = props.project;
+  return (
+    <div style={{ display: "inline-block", width: 300, margin: 10 }}>
+      <h2>{title}</h2>
+      <img src={image} alt="Example app" style={{ width: 200, height: 120 }} />
+      <p>{description}</p>
+      <a href={link}>{link}</a>
+    </div>
+  );
+};
 
-class Projects extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Highlighted Projects</h2>
-        <div>
-          {
-            PROJECTS.map(PROJECT => (
-              <Project key={PROJECT.id} project={PROJECT}/>
-            ))
-          }
-        </div>
-      </div>
-    )
-  }
-}
+const Projects = () => (
+  <div>
+    <h2>Highlighted Projects</h2>
+    <div>
+      {PROJECTS.map(PROJECT => (
+        <Project key={PROJECT.id} project={PROJECT} />
+      ))}
+    </div>
+  </div>
+);
 
 export default Projects;

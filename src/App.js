1d4d7ba0bb2dd2b67a74e2eeeaf2dda374b42e27
fnajer <lexactr@gmail.com>
2019-06-12 11:56:35 +0300

    add main profile image

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Biography from './Biography';
 import Projects from './Projects';
 import SocialProfiles from './SocialProfiles';
+import profile from './assets/profile.png';
 
 class App extends Component {
   state = {
@@ -19,6 +20,7 @@ class App extends Component {
     const { displayBio } = this.state;
     return (
       <div className="App">
+        <img src={profile} alt="profile" className="profile"/>
         <h1>Hello!</h1>
         <p>My name is Alexey. I'm a software engineer</p>
         <p>I'm always looking forward to working on meaningful projects.</p>

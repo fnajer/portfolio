7c312be078cc31c91ded1ae8e06e4104102cbe6c
fnajer <lexactr@gmail.com>
2019-06-13 00:03:17 +0300

    control state of playing audio on Tracks component

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -16,6 +16,29 @@ class Tracks extends Component {
     playingPreviewUrl: null,
   }
 
+  toggleAudio = previewUrl => () => {
+    const audio = new Audio(previewUrl);
+
+    if (!this.isPlaying()) {
+      this.play(audio, previewUrl);
+    } else {
+      this.pause();
+      
+      if (this.wasClickedAnotherAudio(previewUrl)) {
+        this.play(audio, previewUrl);
+      } else {
+        this.setState({ playing: false });
+      }
+    }
+  }
+
+  isPlaying = () => this.state.playing;
+  pause = () => this.state.audio.pause();
+  play = (audio, previewUrl) => {
+    audio.play();
+    this.setState({ playing: true, audio, playingPreviewUrl: previewUrl });
+  }
+  wasClickedAnotherAudio = previewUrl => this.state.playingPreviewUrl !== previewUrl;
 
   render() {
     const { tracks } = this.props;

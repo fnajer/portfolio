862e285e0ea8b0c557a861057d58483d02b85140
fnajer <lexactr@gmail.com>
2019-06-13 19:10:24 +0300

    create markup for start Game; dispatch actions to start and cancel game

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
+import { startGame, cancelGame } from '../../actions/settings';
 
 class Game extends Component {
+  startGame = () => this.props.dispatch(startGame());
+  cancelGame = () => this.props.dispatch(cancelGame());
+
   render() {
-    console.log(this);
-    return <div>Hello</div>;
+    return (
+      <div>
+        <h2>♤ ♡ Evens or Odds ♥ ♢</h2>
+        {
+          this.props.gameStarted ? (
+            <div>
+              <h3>The game is on!</h3>
+              <br />
+              <button onClick={this.cancelGame}>Cancel Game</button>
+            </div>
+          ) : (
+            <div>
+              <h3>A new game awaits</h3>
+              <br />
+              <button onClick={this.startGame}>Start Game</button>
+            </div>          
+          )
+        }
+      </div>
+    );
   }
 }
 

91d808eae9a1a72d54102f87ff8492b98867d31a
fnajer <lexactr@gmail.com>
2019-06-13 19:25:04 +0300

    refactor Game via map dispatch to props, make more cleaner

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,25 +3,23 @@ import { connect } from 'react-redux';
 import { startGame, cancelGame } from '../../actions/settings';
 
 class Game extends Component {
-  startGame = () => this.props.dispatch(startGame());
-  cancelGame = () => this.props.dispatch(cancelGame());
-
   render() {
+    const { gameStarted, startGame, cancelGame} = this.props;
     return (
       <div>
         <h2>♤ ♡ Evens or Odds ♥ ♢</h2>
         {
-          this.props.gameStarted ? (
+          gameStarted ? (
             <div>
               <h3>The game is on!</h3>
               <br />
-              <button onClick={this.cancelGame}>Cancel Game</button>
+              <button onClick={cancelGame}>Cancel Game</button>
             </div>
           ) : (
             <div>
               <h3>A new game awaits</h3>
               <br />
-              <button onClick={this.startGame}>Start Game</button>
+              <button onClick={startGame}>Start Game</button>
             </div>          
           )
         }
@@ -33,7 +31,11 @@ class Game extends Component {
 const mapStateToProps = state => ({
   gameStarted: state.gameStarted,
 });
+const mapDispatchToProps = dispatch => ({
+  startGame: () => dispatch(startGame()),
+  cancelGame: () => dispatch(cancelGame()),
+})
 
-const ConnectComponent = connect(mapStateToProps);
+const ConnectComponent = connect(mapStateToProps, mapDispatchToProps);
 
 export default ConnectComponent(Game);

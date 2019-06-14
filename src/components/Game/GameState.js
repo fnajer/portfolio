e1622b1444ba5776c50dcf5c98265dab79b905c1
fnajer <lexactr@gmail.com>
2019-06-14 21:27:37 +0300

    track on record with save in localstorage

--- src/components/Game/GameState.js
+++ src/components/Game/GameState.js
@@ -1,12 +1,32 @@
 import React from "react";
 import { connect } from "react-redux";
 
+const correctGuessesRecordKey = 'CORRECT_GUESSES_RECORD';
+
+const checkRecord = correctGuesses => {
+  const record = Number(localStorage.getItem(correctGuessesRecordKey));
+
+  if (record < correctGuesses) {
+    localStorage.setItem(correctGuessesRecordKey, correctGuesses);
+    return {
+      record: correctGuesses, isNewRecord: true
+    }
+  }
+  return {
+    record, isNewRecord: false
+  }
+}
+
 const GameState = ({ remaining, correctGuesses }) => {
   const cardText = remaining === 1 ? "card" : "cards";
   const guessText = correctGuesses === 1 ? "guess" : "guesses";
 
+  const { record, isNewRecord } = checkRecord(correctGuesses);
+  const recordLabel = isNewRecord ? "🎉 New Record" : "Record";
+
   return (
     <div>
+      <h3>{recordLabel}: {record}</h3>
       <p>{remaining} {cardText} is remaining</p>
       <p>{correctGuesses} correct {guessText}</p>
     </div>

8c1cb4abfce4dfa69d6b18beec0db93053167577
fnajer <lexactr@gmail.com>
2019-06-17 17:50:17 +0300

    create UserView component for type username

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import PublishMessage from "./PublishMessage";
 import MessageBoard from "./MessageBoard";
+import UserView from "./UserView";
 
 class Chat extends Component {
   render() {
     return (
       <div>
         <h2>Chat Reaction</h2>
+        <UserView />
         <hr/>
         <PublishMessage/>
         <hr />
